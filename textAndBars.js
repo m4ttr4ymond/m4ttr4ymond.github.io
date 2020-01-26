e765f26e2ed0059f4b681a38eda42d43c5ef596b
raymo116 <raymo116@mail.chapman.edu>
2020-01-26 01:16:00 -0800

    mostly fixed library

--- textAndBars.js
+++ textAndBars.js
@@ -1,38 +1,57 @@
 var progressBars = {}
 
-function fnCreateBars(lstBarDicts, blTitle = true) {
+function fnCreateBars(lstBars, blTitle = true, blCollapse = false) {
+    // Sorts the array based on the skill level
+    lstBars.forEach(x => x[1].sort(compare));
+
     var superContainer = $("#container-super")[0];
     var delay = 0;
-    for (var superKey in allBars) {
+    for (var i = 0; i < lstBars.length; ++i) {
 
         var newDiv = document.createElement("div");
         newDiv.classList.add("container");
         newDiv.classList.add("bar-container");
-        newDiv.id = superKey;
 
         superContainer.appendChild(newDiv);
 
+        var subDiv = document.createElement("div");
+        subDiv.className += "collapse"
+        subDiv.id = lstBars[i][0];
+        newDiv.appendChild(subDiv)
+
         if (blTitle) {
             var newH3 = document.createElement("h3");
-            newH3.innerHTML = superKey;
-            newDiv.appendChild(newH3);
-            newDiv.appendChild(document.createElement("hr"));
+            newH3.innerHTML = lstBars[i][0];
+            newDiv.insertBefore(newH3, subDiv);
+            newDiv.insertBefore(document.createElement("hr"), subDiv);
+
+            if(blCollapse) {
+                newH3.setAttribute("data-toggle", "collapse");
+                newH3.className += "resume";
+                newH3.setAttribute("data-target", "#" + lstBars[i][0]);
+                console.log(newH3.getAttribute("data-target"));
+                newH3.onclick = function (e) { fnRefreshBars(e.target.getAttribute("data-target")) };
+            }
         }
 
-        for (var key in lstBarDicts[superKey]) {
-            fnTextAndBars(newDiv, key, lstBarDicts[superKey][key], delay++);
+        for (var k = 0; k < lstBars[i][1].length; k++) {
+            fnTextAndBars(subDiv, lstBars[i][1][k][0], lstBars[i][1][k][1], delay++);
         }
     }
 }
 
-function createBar(current, num, waitTime, intDelayms = 100) {
+function createBar(current, num, waitTime, intDelayms = 100, animate = false) {
+    if(current.id in progressBars) {
+        num = progressBars[current.id][1];
+        progressBars[current.id][0].destroy();
+    }
+
     setTimeout(function() {
-        // console.log(current);
-        progressBars[current.id] =
+        var bar =
             new ProgressBar.Line(current, {
             strokeWidth: 4,
             easing: 'easeInOut',
-            duration: 1000,
+            duration: 1000*animate,
             color: '#FFEA82',
             svgStyle: {width: '100%', height: '100%'},
             from: {color: '#337ca0'},
@@ -41,16 +60,19 @@ function createBar(current, num, waitTime, intDelayms = 100) {
                 bar.path.setAttribute('stroke', state.color);
             }
         });
-        progressBars[current.id].animate(num);
 
-    }, waitTime*intDelayms)
+        progressBars[current.id] = [bar, num];
+
+        progressBars[current.id][0].animate(num);
+
+    }, waitTime*intDelayms*animate)
 }
 
 function fnTextAndBars(htmlObject, strLanguageName, fltBarLevel, fltDelay) {
     var id = strLanguageName.replace(/\s/g, '');
-    id = id.replace("#", "s");
-    id = id.replace("/", "-");
-    id = id.replace("+", "p");
+    id = id.replace(/#/g, "s");
+    id = id.replace(/\//g, "-");
+    id = id.replace(/\+/g, "p");
 
     // Create row
     var row = document.createElement("div");
@@ -86,4 +108,23 @@ function fnTextAndBars(htmlObject, strLanguageName, fltBarLevel, fltDelay) {
     htmlObject.appendChild(row);
 
     createBar(barOutline, fltBarLevel, fltDelay);
+}
+
+function compare(a, b) {
+    return ((a[1] > b[1]) ? -1 : 1);
+}
+
+function fnRefreshBars(strParentID, fltDelayms = 100) {
+    var htmlObject = $(strParentID)[0];
+
+    if (!htmlObject.getAttribute("class").includes("show")) {
+        var delay = 0;
+
+        for (var i = 0; i < htmlObject.childNodes.length; i++) {
+            if(htmlObject.childNodes[i].tagName == 'DIV') {
+                var barHolder = htmlObject.childNodes[i].querySelector('.progress-bar');
+                createBar(barHolder, 0, delay++, fltDelayms, animate = true);
+            }
+        }
+    }
 }
